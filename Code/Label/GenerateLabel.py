d3ce69d51a64d7483e2f56418d2c60ad39b879b8
noob <348880854@qq.com>
2020-02-13 22:08:29 +0800

    Time Interval

--- Code/Label/GenerateLabel.py
+++ Code/Label/GenerateLabel.py
@@ -7,30 +7,45 @@
 #         4、在IPSet中设置 IP地址
 #         5、Go
 
-import os
+
 from scapy.all import *
 
 
 def GenerateLabel(InPcapPath,IpSet,OutPcapPath):
+
+    #================时间设置=============================
+    StartTime = '2017-06-17 15:27:00'
+    OverTime = '2017-06-17 15:29:00'
+    #=====================================================
+
+    StartTimeArray = time.strptime(StartTime, "%Y-%m-%d %H:%M:%S")
+    OverTimeArray = time.strptime(OverTime, "%Y-%m-%d %H:%M:%S")
+    StartTimeStamp = float(time.mktime(StartTimeArray))
+    OverTimeStamp = float(time.mktime(OverTimeArray))
+
+
     print('Now :'+InPcapPath)
     Packets = rdpcap(InPcapPath)
     OutPcap = PcapWriter(OutPcapPath)
     SkipCount = 0
     for Packet in Packets:
-        try:
-            if (Packet['IP'].src in IpSet) and  (Packet['IP'].dst in IpSet):
-                print(repr(Packet))
-                OutPcap.write(Packet)
-        except:
-            SkipCount+=1
-            continue
+        TempStamp = repr(Packet.time)
+        TimeStamp = float(TempStamp[9:-2])
+        if TimeStamp >= StartTimeStamp and TimeStamp <= OverTimeStamp:
+            try:
+                if (Packet['IP'].src in IpSet) and  (Packet['IP'].dst in IpSet):
+                    print(repr(Packet))
+                    OutPcap.write(Packet)
+            except:
+                SkipCount+=1
+                continue
     print(SkipCount)
 
 if __name__ == '__main__':
     InputDir = os.getcwd() + '\InputSet'  #当前输入目录
     OutputDir = os.getcwd() + '\OutputSet' #当前输出目录
     InputName = "06_17_2017-be-20170216-apps-baby.com.SurpriseCollector.pcap"  #设置为当前要处理的文件名
-    OutputName = "Test1.pcap"
+    OutputName = "Test.pcap"
     InPcapPath = InputDir +'\\'+ InputName
     OutPcapPath = OutputDir +'\\' + OutputName    #输出文件名
     IpSet=['52.94.232.33','10.42.0.211']        #填入需获取的IP地址
--- Code/Label/test.py
+++ Code/Label/test.py
@@ -1,25 +1,33 @@
 from scapy.all import *
+import time, datetime
 
+def GenerateLabel(InPcapPath):
 
-def GenerateLabel(InPcapPath,IpSet,OutPcapPath):
+    StartTime = '2017-06-17 15:27:00'
+    OverTime = '2017-06-17 15:29:00'
+    StartTimeArray = time.strptime(StartTime, "%Y-%m-%d %H:%M:%S")
+    OverTimeArray = time.strptime(OverTime, "%Y-%m-%d %H:%M:%S")
+    StartTimeStamp = float(time.mktime(StartTimeArray))
+    OverTimeStamp = float(time.mktime(OverTimeArray))
+
+    print(StartTimeStamp)
+    print(OverTimeStamp)
+
     print('Now :'+InPcapPath)
     Packets = rdpcap(InPcapPath)
-    OutPcap = PcapWriter(OutPcapPath)
-    SkipCount = 0
+    count = 0
     for Packet in Packets:
-
-        print(repr(Packet))
-        OutPcap.write(Packet)
+        TempStamp = repr(Packet.time)
+        TimeStamp = float(TempStamp[9:-2])
+        if TimeStamp>=StartTimeStamp and TimeStamp<=OverTimeStamp:
+            count+=1
+    print(count)
 
     #print(SkipCount)
 
 if __name__ == '__main__':
     InputDir = os.getcwd() + '\InputSet'  #当前输入目录
-    OutputDir = os.getcwd() + '\OutputSet' #当前输出目录
-    InputName = "Tuesday-WorkingHours" \
-                ".pcap"  #设置为当前要处理的文件名
-    OutputName = "Test.pcap"
+    #OutputDir = os.getcwd() + '\OutputSet' #当前输出目录
+    InputName = "06_17_2017-be-20170216-apps-baby.com.SurpriseCollector.pcap"  #设置为当前要处理的文件名
     InPcapPath = InputDir +'\\'+ InputName
-    OutPcapPath = OutputDir +'\\' + OutputName    #输出文件名
-    IpSet=['52.94.232.33','10.42.0.211']        #填入需获取的IP地址
-    GenerateLabel(InPcapPath,IpSet,OutPcapPath)
+    GenerateLabel(InPcapPath)
